5a6f931b8801523306a9a62c60e1456a488835d2
Anu451 <devanu451@gmail.com>
2026-06-19 21:33:52 +0530

    Implement dynamic sign-in/sign-up toggle and improve form structure in Login component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
 import Header from './Header';
+import { useState } from 'react';
 
 const Login = () => {
+	const [isSignIn, setIsSignIn] = useState(true);
 	return (
 		<div className=" relative">
 			<Header />
@@ -11,11 +12,20 @@ const Login = () => {
 					alt=""
 				/>
 			</div>
-			<form
-				className=" absolute top-[50%] left-[50%] transform -translate-x-1/2 -translate-y-1/2 bg-black/70 p-10 rounded-md flex flex-col text-white gap-3 w-3/12"
-				action=""
-			>
-				<h2 className=" text-3xl  font-semibold mb-5"> Login</h2>
+			<form className=" absolute top-[50%] left-[50%] transform -translate-x-1/2 -translate-y-1/2 bg-black/70 p-10 rounded-md flex flex-col text-white gap-5 w-3/12">
+				<h2 className=" text-3xl  font-semibold mb-5">
+					{isSignIn ? 'Sign In' : 'Sign Up'}
+				</h2>
+				{!isSignIn && (
+					<input
+						type="text"
+						name="name"
+						id="name"
+						placeholder="Enter your name "
+						className=" bg-zinc-800 px-5 py-2 rounded focus-visible:outline-0"
+					/>
+				)}
+
 				{/* <label htmlFor="email-id" className=" text-white">
 					Email ID
 				</label> */}
@@ -38,15 +48,25 @@ const Login = () => {
 					className=" bg-zinc-800 px-5 py-2 rounded focus-visible:outline-0"
 				/>
 				<button className="mt-3 bg-red-600 text-white rounded py-2">
-					Sign In
+					{isSignIn ? 'Sign In' : 'Sign Up'}
 				</button>
-				<div className=" align-middle text-zinc-500">
-					<input type="checkBox" id="rememberMe" className=" mr-2" />
-					<label htmlFor="rememberMe">Remember me</label>
-				</div>
+				{isSignIn && (
+					<div className=" align-middle text-zinc-500">
+						<input type="checkBox" id="rememberMe" className=" mr-2" />
+						<label htmlFor="rememberMe">Remember me</label>
+					</div>
+				)}
+
 				<div className=" mt-5 text-zinc-500">
 					<p>
-						New to Netflix? <span className=" text-white">Sign up now</span>
+						{isSignIn ? 'New to netflix ? ' : 'Already a member ?'}
+						<button
+							className=" text-white cursor-pointer pl-2"
+							onClick={() => setIsSignIn(!isSignIn)}
+							type="button"
+						>
+							{isSignIn ? 'Sign Up' : 'Sign In'}
+						</button>
 					</p>
 					<p>This page is protected by google capche </p>
 				</div>
